bd582aecce98d2cfe915c776bb57d8c3b90cb984
Daksh Sagar <daksh@Dakshs-MacBook-Air.local>
2023-11-10 19:26:50 +0530

    create rightsidebar
    Signed-off-by: Daksh Sagar <daksh@Dakshs-MacBook-Air.local>

---
+++ client/components/shared/renderTag/RenderTag.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import { Badge } from '@/components/ui/badge'
+
+type Props = {
+  _id: number
+  name: string
+  showCount?: boolean
+  count?: number
+}
+
+export function RenderTag({ _id, name, count, showCount = false }: Props) {
+  return (
+    <Link href={`/tags/${_id}`} className='flex justify-between gap-2'>
+      <Badge className='subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase'>
+        {name}
+      </Badge>
+
+      {showCount && <p className='small-medium text-dark500_light700'>{count}</p>}
+    </Link>
+  )
+}
