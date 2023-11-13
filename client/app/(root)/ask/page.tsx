119f87ab4c4b2ed3a61759caa82b5ce107af8bcf
Daksh Sagar <daksh@Dakshs-MacBook-Air.local>
2023-11-13 16:58:40 +0530

    add QuestionForm component
    Signed-off-by: Daksh Sagar <daksh@Dakshs-MacBook-Air.local>

--- client/app/(root)/ask/page.tsx
+++ client/app/(root)/ask/page.tsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import { QuestionForm } from '@/components/forms/QuestionForm'
 
-export default function Ask() {
-  return <div>Ask</div>
+export default function Page() {
+  return (
+    <div>
+      <h1 className='h1-bold text-dark100_light900'>Ask a Question</h1>
+
+      <div className='mt-9'>
+        <QuestionForm />
+      </div>
+    </div>
+  )
 }
