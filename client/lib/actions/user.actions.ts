8407d4365d275181e3f16785cf1588a887c3820d
Daksh Sagar <daksh@Dakshs-MacBook-Air.local>
2023-11-14 22:41:32 +0530

    add user actions
    Signed-off-by: Daksh Sagar <daksh@Dakshs-MacBook-Air.local>

--- client/lib/actions/user.actions.ts
+++ client/lib/actions/user.actions.ts
@@ -2,14 +2,67 @@
 
 import { connectToDb } from '@/database/db'
 import { User } from '@/database/user.model'
+import { CreateUserParams, DeleteUserParams, GetUserByIdParams, UpdateUserParams } from './shared.types'
+import { revalidatePath } from 'next/cache'
+import { Question } from '@/database/question.model'
 
-export async function getUserById(params: any) {
+export async function getUserById(params: GetUserByIdParams) {
   try {
     await connectToDb()
 
     const { userId } = params
 
     const user = await User.findOne({ clerkId: userId })
+    return user
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export async function createUser(params: CreateUserParams) {
+  try {
+    await connectToDb()
+    const user = new User(params)
+    await user.save()
+    return user
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    await connectToDb()
+
+    const { clerkId, path, updateData } = params
+
+    const mongoUser = await User.findOneAndUpdate({ clerkId }, updateData, { new: true })
+    revalidatePath(path)
+    return mongoUser
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    await connectToDb()
+
+    const { clerkId } = params
+
+    const user = await User.findOneAndDelete({ clerkId })
+
+    if (!user) {
+      return
+    }
+
+    // also delete other data associated with the user (questions, answers, interactions)
+
+    await Question.deleteMany({ author: user._id })
+
     return user
   } catch (error) {
     console.error(error)
