3cc5ede012105ee17288631ab35e188e343bc1cc
Daksh Sagar <daksh@Dakshs-MacBook-Air.local>
2023-12-06 23:09:57 +0530

    setup db connection pool using pgx
    Signed-off-by: Daksh Sagar <daksh@Dakshs-MacBook-Air.local>

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const version = "1.0.0"
@@ -14,6 +17,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		// maxIdleConns int32 - not supported by pgxpool
+		maxIdleTime time.Duration
+	}
 }
 
 type application struct {
@@ -26,12 +35,27 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "App environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://root:pa55word@localhost:5432/heapharbor?sslmode=disable", "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
 
 	flag.Parse()
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(&cfg)
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	app := &application{
 		config: cfg,
-		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
+		logger: logger,
 	}
 
 	srv := &http.Server{
@@ -45,8 +69,31 @@ func main() {
 
 	app.logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.logger.Error(err.Error())
 	os.Exit(1)
 
+}
+
+func openDB(cfg *config) (*pgxpool.Pool, error) {
+	dbConfig, _ := pgxpool.ParseConfig(cfg.db.dsn)
+	dbConfig.MaxConns = int32(cfg.db.maxOpenConns)
+	dbConfig.MaxConnIdleTime = cfg.db.maxIdleTime
+
+	db, err := pgxpool.NewWithConfig(context.Background(), dbConfig)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the pgxpool.Pool connection pool.
+	return db, nil
 }
--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,16 @@ module github.com/daksh-sagar/HeapHarbor/api
 
 go 1.21.4
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/jackc/pgx/v5 v5.5.0
+	github.com/julienschmidt/httprouter v1.3.0
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
