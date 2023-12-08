ed00f1fb1c922e70d6019a8fe127979454ea13db
Daksh Sagar <daksh@Dakshs-MacBook-Air.local>
2023-12-08 23:31:04 +0530

    add questionsUpvotes & questionsDownvotes table migrations
    Signed-off-by: Daksh Sagar <daksh@Dakshs-MacBook-Air.local>

---
+++ api/migrations/000005_create-questions-upvotes-table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE questionsUpvotes (
+  questionId INT REFERENCES questions(_id) ON DELETE CASCADE NOT NULL,
+  userId INT REFERENCES users(_id) ON DELETE CASCADE NOT NULL,
+  UNIQUE (questionId, userId)
+);
