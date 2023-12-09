31324454cbab5f0ca6fe4d1e2c4229a6065e417c
Daksh Sagar <daksh@Dakshs-MacBook-Air.local>
2023-12-09 22:50:51 +0530

    add answerUvotes and answersDownvotes table migrations
    Signed-off-by: Daksh Sagar <daksh@Dakshs-MacBook-Air.local>

---
+++ api/migrations/000009_create-answers-downvotes-table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE answersDownvotes (
+  answerId BIGINT REFERENCES answers(_id) ON DELETE CASCADE NOT NULL,
+  userId BIGINT REFERENCES users(_id) ON DELETE CASCADE NOT NULL,
+  UNIQUE (answerId, userId)
+);
