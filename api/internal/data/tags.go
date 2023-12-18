b125c5832c0137f0086ecc1c6d1dddde115062a5
Daksh Sagar <daksh@Dakshs-MacBook-Air.local>
2023-12-18 11:17:30 +0530

    implement methods to get user's saved questions, popular tags, questions by tag
    Signed-off-by: Daksh Sagar <daksh@Dakshs-MacBook-Air.local>

--- api/internal/data/tags.go
+++ api/internal/data/tags.go
@@ -52,3 +52,43 @@ func (m *TagModel) GetPopular() ([]*Tag, error) {
 
 	return popularTags, nil
 }
+
+func (m *TagModel) GetAll() ([]*Tag, error) {
+	query := `
+		SELECT
+				_id,
+				name,
+				COUNT(qt.questionId) AS question_count
+		FROM
+				tags t
+		LEFT JOIN
+				questionsTags qt ON t._id = qt.tagId
+		GROUP BY
+				t._id
+		ORDER BY
+				question_count DESC;
+  `
+
+	rows, err := m.DB.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var tags []*Tag
+
+	for rows.Next() {
+		var tag Tag
+		if err := rows.Scan(&tag.Id, &tag.Name, &tag.QuestionsCount); err != nil {
+			return nil, fmt.Errorf("error scanning tag row: %w", err)
+		}
+		tags = append(tags, &tag)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error iterating over tag rows: %w", err)
+	}
+
+	return tags, nil
+}
